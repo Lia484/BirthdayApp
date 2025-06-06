c726cca049bfd03b1cd8c0f187d4e618c9be414b
Lia <lworland28@stjoesphacademy.org>
2025-06-06 11:44:55 -0500

    finfished step 3

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-       
-    }//end body
-}// end struct
+    @State private var friends: [Friend] = [
+        Friend(name: "Elton Lin", birthday: .now),
+        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
+        
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+var body: some View {
+    NavigationStack {
+        List(friends, id: \.name) { friend in
+            HStack {
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            }
+        }
+        .navigationTitle("Bithdays")
+        .safeAreaInset(edge: .bottom) {
+            VStack(alignment: .center, spacing: 20) {
+                Text("New Birthday")
+                    .font(.headline)
+                DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                            TextField("Name", text: $newName)
+                                .textFieldStyle(.roundedBorder)
+                        }
+                Button("Save") {
+                           let newFriend = Friend(name: newName, birthday: newBirthday)
+                           friends.append(newFriend)
+                           newName = ""
+                        newBirthday = Date.now
+                       }
+                       .bold()
+                   }
+                .padding()
+                .background(.bar)
+               }
+            }
+        }
+        }
+  
+
 
 #Preview {
     ContentView()
---
+++ BirthdayApp/friend.swift
@@ -0,0 +1,19 @@
+//
+//  friend.swift
+//  BirthdayApp
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import Foundation
+import SwiftData
+
+
+class Friend {
+    var name: String
+    var birthday:Date
+init (name: String, birthday: Date){
+    self.name = name
+    self.birthday = birthday
+    }
+}
